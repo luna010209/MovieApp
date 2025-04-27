b569104e3dac07c660e7ef794046f24aa6d3bd75
Lunado <liendhhha140217@gmail.com>
2025-04-27 23:36:57 +0900

    Complete homepage

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { FontAwesome } from '@expo/vector-icons'
 
 const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) => {
   return (
@@ -12,10 +13,22 @@ const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) 
             `https://image.tmdb.org/t/p/w500${poster_path}`:
             `https://placehold.co/600x400/1a1a1a/ffffff.png`
           }}
-          className='w-full h-52 rounded-lg'
+          className='w-full h-52 rounded-lg mt-3'
           resizeMode='cover'
         />
-        <Text className='text-white font-bold text-center mb-2'>{title}</Text>
+        <Text className='text-white font-bold mb-1' numberOfLines={1}>{title}</Text>
+        <View className='flex-row items-center justify-start gap-x-1'>
+          <FontAwesome name="star" size={15} color="yellow"/>
+          <Text className='text-white font-bold uppercase'>{Math.round(vote_average/2*10)/10}</Text>
+        </View>
+        <View className='flex-row items-center justify-between'>
+          <Text className='text-xs text-secondary font-medium mt-1'>
+            {release_date?.split('-')[0]}
+          </Text>
+          {/* <Text className='text-secondary text-xs'>
+            Movie
+          </Text> */}
+        </View>
       </TouchableOpacity>
     </Link>
   )
